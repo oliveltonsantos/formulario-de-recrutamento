b67857c24f1e8c2c8d5f9fcc52f3cd5ebc71acaa
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-03-04 15:18:28 -0300

    Formulário

---
+++ cadastro.php
@@ -0,0 +1,3 @@
+Sua candidatura foi realizada com sucesso!
+
+Se o seu perfil for compatível com a vaga entraremos em contato, caso contrário o seu currículo ficará em nossa banco de dados para futuras oportunidades.
